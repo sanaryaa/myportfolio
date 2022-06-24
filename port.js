61ad532d7bd58eaf67480c1ba7e82a5a3a7515d6
sanarya1 <sanaryahamakarim1@gmail.om>
2022-06-24 14:00:20 +0200

    add skill section

---
+++ port.js
@@ -0,0 +1,27 @@
+let current=0;
+showCard(current);
+function nextCard(m){
+    current+=1;
+    showCard(current);
+}
+function prevCard(m){
+    current-=1;
+    showCard(current);
+}
+function currCard(m) {
+    current = m;
+    showCard(current);
+  }
+function showCard(m){
+    let cards=document.getElementsByClassName("box");
+    if (m<0) {
+        m=0;
+    }
+    if(m>cards.length-1){
+        m=cards.length-1;
+    }
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].style.display="none";
+    }
+    cards[m].style.display="block";
+}
